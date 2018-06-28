0092e0446b4a11aa3a7d9f2da0816af83a9de5c7
何仕敬 <hesj1139@13322ty.com>
2018-06-28 11:38:55 +0800

    EventBus公共方法优化

--- src/plugin/event-bus.js
+++ src/plugin/event-bus.js
@@ -4,14 +4,18 @@ export default {
       data: {
         global: {}
       },
+      watch: {
+        'global.test' () {
+          console.log(this.global.test)
+        }
+      },
       methods: {
         setData (key, data) {
           // 由于Vue 不允许在已经创建的实例上动态添加新的根级响应式属性(root-level reactive property)
           // 此方法方便将数据写入全局变量中
           // this.$set(this.global, key, data)
-          this.global = Object.assign({}, this.global, {
-            key: data
-          })
+          let result = key ? { [key]: data } : data
+          this.global = Object.assign({}, this.global, result)
         }
       }
     })
