3acb2af52d213e835f9807d23a751da5b419385a
何仕敬 <hesj1139@13322ty.com>
2018-06-28 11:14:16 +0800

    添加公共setData方法，默认添加至global对象中

--- src/plugin/event-bus.js
+++ src/plugin/event-bus.js
@@ -2,8 +2,16 @@ export default {
   install (Vue) {
     Vue.prototype.$eventBus = new Vue({
       data: {
-        global: {
-          a: '1212121'
+        global: {}
+      },
+      methods: {
+        setData (key, data) {
+          // 由于Vue 不允许在已经创建的实例上动态添加新的根级响应式属性(root-level reactive property)
+          // 此方法方便将数据写入全局变量中
+          // this.$set(this.global, key, data)
+          this.global = Object.assign({}, this.global, {
+            key: data
+          })
         }
       }
     })
