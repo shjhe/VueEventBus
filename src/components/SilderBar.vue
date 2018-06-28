06dfba79989cef99fcac2592e983215bf4e4ce35
何仕敬 <hesj1139@13322ty.com>
2018-06-28 12:28:33 +0800

    菜单栏组件提交

---
+++ src/components/SilderBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-aside class="silder-bar" width="200px">
+    <el-menu router>
+      <el-menu-item-group>
+        <template slot="title">分组一</template>
+        <el-menu-item index="/">数据响应</el-menu-item>
+        <el-menu-item index="/12">选项2</el-menu-item>
+      </el-menu-item-group>
+    </el-menu>
+  </el-aside>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="scss">
+  .silder-bar{
+    min-height: 500px;
+    .el-menu{
+      height: 100%;
+    }
+  }
+</style>
