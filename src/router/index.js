bf246f31acd7e75f68ec4a37f43db8556674fa9d
何仕敬 <hesj1139@13322ty.com>
2018-06-28 11:40:40 +0800

    文件路径修改

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ export default new Router({
     {
       path: '/',
       name: 'DataTest',
-      component: () => import('@/views/DataTest')
+      component: () => import('@/views/datatest')
     }
   ]
 })
---
+++ src/views/datatest/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="data-test">
+    <el-row>
+      <el-col :span="10">
+        <el-button type="primary" @click="handleChange">修改数据</el-button>
+      </el-col>
+      <el-col :span="10" :offset="4">
+        {{test}}
+        {{$eventBus.global.test}}
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  },
+  computed: {
+    test () {
+      return this.$eventBus.global && this.$eventBus.global.test
+    }
+  },
+  created () {
+    window.tyler = this
+    this.initEventBus()
+  },
+  methods: {
+    initEventBus () {
+      this.$eventBus.setData('test', {
+        a: '1'
+      })
+    },
+    handleChange () {
+      this.$eventBus.global.test.a = Math.random()
+    }
+  }
+}
+</script>
